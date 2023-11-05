c8890fc8cbbee51931be92ced6b91e9b58362727
JoaoSiqueira10 <joao.guilherme100@hotmail.com>
2023-11-05 09:41:00 -0300

    Parte 1 - Criando as funcoes da calculadora

---
+++ TesteNewTalents/TesteNewTalents.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using NewTalents;
+
+namespace TesteNewTalents
+{
+    public class TesteNewTalents
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculadora calc = new Calculadora();
+        }
+    }
+}
